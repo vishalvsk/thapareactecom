6ebf01dc318b66fb26289a1231db6c3510e4ce9e
vishal Karale <vishalkaralevsk@gmail.com>
2023-01-25 14:32:01 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import  Contact  from "./Contact";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
 
 const App = () => {
   return (
-    <div>
-      <h3> Welcome to Vishal React E-Commerce Website</h3>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+          <Route exact path="/products" element={<Products />}></Route>
+          <Route
+            exact
+            path="/singleproduct"
+            element={<SingleProduct />}
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
